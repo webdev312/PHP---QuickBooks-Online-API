ec1fd0ff512da886adc2d95efba35401bfd2de44
mm_dev <bowwck@gmail.com>
2019-03-14 10:00:00 -0800

    ui bug fix

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <header>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/index1.css">
 </header>
 <body>
     <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
